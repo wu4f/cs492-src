b01c61bb88727ff976910812819ff0a4341e0f4b
Wu-chang Feng <wuchangfeng@bitbucket.org>
2018-02-05 18:28:19 -0800

    path

---
+++ afl/04_path_fuzz/strcmp_based.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+        char ptr[20];
+        char* p;
+
+        if(argc>1){
+                FILE *fp = fopen(argv[1], "r");
+                p = fgets(ptr, sizeof(ptr), fp);
+        }
+        else{
+                p = fgets(ptr, sizeof(ptr), stdin);
+        }
+        if (strcmp(ptr,"foob") == 0 )
+              abort();
+	else
+              printf("%s", ptr);
+        return 0;
+}
