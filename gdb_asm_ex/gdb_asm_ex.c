d7cc7d98bacf7ef86f16b002ffd0e9af083dd1ea
Wu-chang Feng <wuchang@pdx.edu>
2024-03-01 14:25:07 -0800

    move over to afl++

--- gdb_asm_ex/gdb_asm_ex.c
+++ gdb_asm_ex/gdb_asm_ex.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int pass_code = 10;
 int user_code;
-char e[] = "Enter ";
-char t[] = "the ";
-char p[] = "password: ";
 void sub() {
 	user_code++;
 	pass_code++;
 	return;
 }
+char e[] = "Enter ";
+char t[] = "the ";
+char p[] = "password: ";
 int main() {
 	user_code = 0;
 
-	printf("%s%s%s\n",e,t,p);
+	printf("%s%s%s",e,t,p);
 	scanf("%d",&user_code);
 	sub();
 	if (user_code == pass_code)
-		printf("Good Job!\n");
+		printf("Good Job.\n");
 	else
 		printf("Try again.\n");
 }
